1dc788d160041c894664eac39c168681a61537b1
AJULUMEL <azruljoss@gmail.com>
2025-04-14 09:43:23 +0700

    praktikum 3 js8

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\Log;
+use Barryvdh\DomPDF\Facade\Pdf;
 class BarangController extends Controller
 {
     public function index()
@@ -392,5 +393,20 @@ class BarangController extends Controller
 
         $writer->save('php://output');
         exit;
+    }
+
+    public function export_pdf()
+    {
+        $barang = BarangModel::select('kategori_id','barang_kode','barang_nama','harga_beli','harga_jual')
+            ->orderBy('kategori_id')
+            ->with('kategori')
+            ->get();
+
+        $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+        $pdf->setPaper('a4', 'portrait');
+        $pdf->setOption("isRemoteEnbled", true);
+        $pdf->render();
+
+        return $pdf->stream('Data Barang'.date('Y-m-d H:i:s').'.pdf');
     }
 }
